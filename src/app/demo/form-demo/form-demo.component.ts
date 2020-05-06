8a0c05f0e8a95b4f95c6dcf923f52a7ea10e8012
Martin Meyer <martin.meyer@inftec.ch>
2020-05-06 15:17:52 +0200

    Added alert and actionSheet

--- src/app/demo/form-demo/form-demo.component.ts
+++ src/app/demo/form-demo/form-demo.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, Validators } from "@angular/forms";
+import { ToastController, ActionSheetController } from "@ionic/angular";
 
 @Component({
   selector: "app-form-demo",
   templateUrl: "./form-demo.component.html",
   styleUrls: ["./form-demo.component.scss"],
 })
-export class FormDemoComponent implements OnInit {
+export class FormDemoComponent {
   demoForm = this.fb.group({
     company: [{ value: "InfTec", disabled: true }],
     firstName: ["", Validators.required],
@@ -20,7 +21,61 @@ export class FormDemoComponent implements OnInit {
     }),
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private actionSheetController: ActionSheetController,
+    private fb: FormBuilder,
+    private toastController: ToastController
+  ) {}
 
-  ngOnInit() {}
+  async submit() {
+    await this.showToast("Submitting Data (not really 😜)");
+  }
+
+  private async showToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 1000,
+    });
+    await toast.present();
+  }
+
+  async showActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: "Albums",
+      buttons: [
+        {
+          text: "Delete",
+          role: "destructive",
+          icon: "trash",
+          handler: () => this.showClickInfo("Delete"),
+        },
+        {
+          text: "Share",
+          icon: "share",
+          handler: () => this.showClickInfo("Share"),
+        },
+        {
+          text: "Play (open modal)",
+          icon: "play",
+          handler: () => this.showClickInfo("Play"),
+        },
+        {
+          text: "Favorite",
+          icon: "heart",
+          handler: () => this.showClickInfo("Favorite"),
+        },
+        {
+          text: "Cancel",
+          icon: "close",
+          role: "cancel",
+          handler: () => this.showClickInfo("Cancel"),
+        },
+      ],
+    });
+    await actionSheet.present();
+  }
+
+  private showClickInfo(type: string) {
+    this.showToast(`${type} clicked`);
+  }
 }

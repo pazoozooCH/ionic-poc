1e3d7878b10e4f73e44df969bce409b684c883e2
Martin Meyer <martin.meyer@inftec.ch>
2020-05-15 22:23:39 +0200

    camera: Improved error handling

--- src/app/demo/camera-demo/camera-demo.component.ts
+++ src/app/demo/camera-demo/camera-demo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Plugins, CameraResultType, CameraSource } from "@capacitor/core";
 import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
+import { ToastController } from "@ionic/angular";
 
 @Component({
   selector: "app-camera-demo",
@@ -9,18 +10,34 @@ import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 })
 export class CameraDemoComponent {
   photo: SafeResourceUrl;
+  showInfo = true;
 
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor(
+    private sanitizer: DomSanitizer,
+    private toastController: ToastController
+  ) {}
+
   async takePicture() {
-    const image = await Plugins.Camera.getPhoto({
-      quality: 100,
-      allowEditing: false,
-      resultType: CameraResultType.DataUrl,
-      source: CameraSource.Camera,
-    });
+    try {
+      const image = await Plugins.Camera.getPhoto({
+        quality: 100,
+        allowEditing: false,
+        resultType: CameraResultType.DataUrl,
+        source: CameraSource.Camera,
+      });
 
-    this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(
-      image && image.dataUrl
-    );
+      this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(
+        image && image.dataUrl
+      );
+
+      this.showInfo = false;
+    } catch (error) {
+      console.error(error);
+      const toast = await this.toastController.create({
+        duration: 2000,
+        message: `Couldn't take photo: ${error}`,
+      });
+      await toast.present();
+    }
   }
 }

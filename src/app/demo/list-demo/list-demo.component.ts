6bc20c4e157fa93dbcfd7f6cfe5c08ffaf9a7cfd
Martin Meyer <martin.meyer@inftec.ch>
2020-05-15 23:15:16 +0200

    list: Reordering

--- src/app/demo/list-demo/list-demo.component.ts
+++ src/app/demo/list-demo/list-demo.component.ts
@@ -1,16 +1,59 @@
 import { Component, OnInit } from "@angular/core";
 import { ToastController } from "@ionic/angular";
 
+interface ReorderingItem {
+  index: number;
+  position: number;
+  color: string;
+}
+
 @Component({
   selector: "app-list-demo",
   templateUrl: "./list-demo.component.html",
   styleUrls: ["./list-demo.component.scss"],
 })
 export class ListDemoComponent {
+  private static colors = [
+    "primary",
+    "secondary",
+    "tertiary",
+    "success",
+    "warning",
+    "danger",
+  ];
+  reorderingItems: ReorderingItem[] = Array.from(new Array(5)).map((_, i) => ({
+    index: i,
+    position: i,
+    color: ListDemoComponent.colors[i % ListDemoComponent.colors.length],
+  }));
+
+  reordering = false;
+
+  reorderingItems2 = Array.from(new Array(5)).map(
+    (_, i) => ListDemoComponent.colors[i % ListDemoComponent.colors.length]
+  );
+
   constructor(private toastController: ToastController) {}
 
   selected(text: string) {
     this.showToast(text);
+  }
+
+  doReorder(event: CustomEvent) {
+    const detail = event.detail;
+    const from: number = detail.from;
+    const to: number = detail.to;
+
+    const newOrder = [...this.reorderingItems];
+    newOrder.splice(to, 0, newOrder.splice(from, 1)[0]);
+
+    this.reorderingItems = newOrder;
+
+    event.detail.complete();
+  }
+
+  doReorder2(event: CustomEvent) {
+    event.detail.complete();
   }
 
   private async showToast(message: string) {

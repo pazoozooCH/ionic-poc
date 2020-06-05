cce828cd8b0261ed3149d6d069a279675d7c508a
Martin Meyer <martin.meyer@inftec.ch>
2020-06-05 22:18:49 +0200

    Added startup time

--- src/app/main/welcome/welcome.component.ts
+++ src/app/main/welcome/welcome.component.ts
@@ -2,20 +2,23 @@ import { Component, OnInit } from "@angular/core";
 import { environment } from "src/environments/environment";
 import { AuthService } from "src/app/core/auth/auth.service";
 import { startWith } from "rxjs/operators";
+import { AppService } from "../app.service";
 
 @Component({
   selector: "app-welcome",
   templateUrl: "./welcome.component.html",
   styleUrls: ["./welcome.component.scss"],
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent {
   version = environment.build.version;
   buildTime = environment.build.buildTime;
   localBuildTime = Date.parse(environment.build.buildTime);
+  startupTime = this.appService.startupTime;
 
   loggedIn$ = this.authService.canActivateProtectedRoutes$;
 
-  constructor(private authService: AuthService) {}
-
-  ngOnInit() {}
+  constructor(
+    private appService: AppService,
+    private authService: AuthService
+  ) {}
 }
